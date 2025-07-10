6f8da43ad1a37d5408eb1aed9e9672cfedc3d806
goldenshoes001 <sebim1993@googlemail.com>
2025-07-10 16:04:31 +0200

    AUfgabe von heute mit Pravur erledigt

--- lib/solutions/chapter_4/subchapter_5/lesson_1/s_4_5_1_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_1/s_4_5_1_1.dart
@@ -4,6 +4,15 @@ class S4511 extends StatelessWidget {
   const S4511({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return HalloWorld();
+  }
+}
+
+class HalloWorld extends StatelessWidget {
+  const HalloWorld({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hello World");
   }
 }
--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -4,6 +4,31 @@ class S4522 extends StatelessWidget {
   const S4522({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyStatelessWidget();
   }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("Hello Sebastian"),
+        SizedBox(
+          height: 20,
+        ),
+        ElevatedButton(
+          onPressed: onPressed,
+          child: Text("Klick mich"),
+          style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: const Color.fromARGB(255, 179, 1, 255)),
+        ),
+      ],
+    );
+  }
+
+  void onPressed() {}
 }
